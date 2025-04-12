4ce9f722e25bafaee808cc7aad03bdaad0e8d43e
Greg <gregcourt10@gmail.com>
2025-04-12 10:18:13 +0100

    formatting

--- keyvault-rg/outputs.tf
+++ keyvault-rg/outputs.tf
@@ -1,13 +1,13 @@
-output "resource_group_name" { 
- value = azurerm_resource_group.this.name
+output "resource_group_name" {
+  value = azurerm_resource_group.this.name
 }
 
-output "resource_group_id" { 
- value = azurerm_resource_group.this.id 
+output "resource_group_id" {
+  value = azurerm_resource_group.this.id
 }
 
-output "keyvaults" { 
- value = { 
-  for kv in azurerm_key_vault.this : kv.name => kv.vault_uri 
- } 
+output "keyvaults" {
+  value = {
+    for kv in azurerm_key_vault.this : kv.name => kv.vault_uri
+  }
 }
